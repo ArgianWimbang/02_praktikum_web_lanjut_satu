3abc94792b9fb37c5540b713e23f11798b38af41
Argian13 <argianvarell12@gmail.com>
2021-02-17 19:44:34 +0700

    Commit Kedua

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    echo "Selamat Datang";
+    });
+
+Route::get('/about',  function () {
+    echo "Argian Wimbang Ekivarel_1941720113";
+    });
+
+Route::get('/articles/{id}',  function ($id) {
+    echo "Halaman Artikel dengan ID".$id;
+    });
+
